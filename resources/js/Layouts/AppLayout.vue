e413422b5b3262a6fa3e7e185d3acf9d9ce1aa10
Karthyyyy <d.mccarthyallen@gmail.com>
2024-04-13 15:48:03 +0100

    Initial setup

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <Head :title="title" />
+    <div class="md:p-10 py-10">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { Head, Link, router } from '@inertiajs/vue3';
+
+defineProps({
+    title: String,
+});
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/Pages/Quotes.vue
@@ -0,0 +1,18 @@
+<template>
+    <AppLayout title="Get Quotes">
+        <div class="md:w-1/2 mx-auto bg-gray-800 p-5 md:rounded-xl">
+            <h1 class="text-2xl">Quotes From Kanye</h1>
+            <div class="h-px w-full bg-gray-600 my-4"></div>
+
+            
+        </div>
+    </AppLayout>
+</template>
+
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<style>
+
+</style>
